112e2632d38e24534bc51c5448f88112afb53905
hamabu0531 <hamabu0531@icloud.com>
2025-02-10 23:34:11 +0900

    移動時のアニメーションを追加(横移動、しゃがみ、ジャンプ、ジャンプ攻撃)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,9 +14,15 @@ public class PlayerController : MonoBehaviour
 
     // 変数
     private float speed = 5.0f;
+    private Animator playerAnim;
+
+    // アニメーション
+    public Sprite idleSprite, crouchSprite;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        // フレームレートの設定
         Application.targetFrameRate = 60;
         QualitySettings.vSyncCount = 0;
         // PlayerのActionInputを取得
@@ -35,6 +41,9 @@ public class PlayerController : MonoBehaviour
         attackMKInput.Enable();
         attackLPInput.Enable();
         attackMPInput.Enable();
+
+        // アニメーションの設定
+        playerAnim = GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -42,81 +51,113 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 move = moveInput.ReadValue<Vector2>();
 
-
+        // テンキー表示
+        int input = 0;
+        if(move.x > 0.5f)
+        {
+            if (move.y > 0.5f)
+            {
+                input = 9;
+            }
+            else if (move.y < -0.5f)
+            {
+                input = 3;
+            }
+            else
+            {
+                input = 6;
+            }
+        }
+        else if (move.x < -0.5f)
+        {
+            if(move.y > 0.5f)
+            {
+                input = 7;
+            }
+            else if (move.y < -0.5f)
+            {
+                input = 1;
+            }
+            else
+            {
+                input = 4;
+            }
+        }
+        else
+        {
+            if (move.y > 0.5f)
+            {
+                input = 8;
+            }
+            else if (move.y < -0.5f)
+            {
+                input = 2;
+            }
+            else
+            {
+                input = 5;
+            }
+        }
 
         // 攻撃
         if (attackLKInput.triggered)
         {
             Debug.Log("弱キック");
+            if (!isGrounded)
+            {
+                JumpAttack();
+            }
         }
         if(attackMKInput.triggered)
         {
             Debug.Log("中キック");
+            if (!isGrounded)
+            {
+                JumpAttack();
+            }
         }
         if(attackLPInput.triggered)
         {
             Debug.Log("弱パンチ");
+            if (!isGrounded)
+            {
+                JumpAttack();
+            }
         }
         if (attackMPInput.triggered)
         {
             Debug.Log("中パンチ");
+            if (!isGrounded)
+            {
+                JumpAttack();
+            }
         }
-
-        // 移動
-        float x = move.x;
-        float y = move.y;
 
         // ジャンプ
-        if (y > 0.5f && isGrounded)
+        if (isGrounded && (input == 7 || input == 8 || input == 9))
         {
-            // 前ジャンプ
-            if (x > 0.5f)
-            {
-                Rigidbody2D rb = GetComponent<Rigidbody2D>();
-                rb.AddForce(new Vector2(10, 10), ForceMode2D.Impulse);
-                isGrounded = false;
-            }
-            // 後ろジャンプ
-            else if (x < -0.5f)
-            {
-                Rigidbody2D rb = GetComponent<Rigidbody2D>();
-                rb.AddForce(new Vector2(-10, 10), ForceMode2D.Impulse);
-                isGrounded = false;
-            }
-            // 垂直ジャンプ
-            else
-            {
-                Rigidbody2D rb = GetComponent<Rigidbody2D>();
-                rb.AddForce(new Vector2(0, 10), ForceMode2D.Impulse);
-                isGrounded = false;
-            }
+            Jumping(input);
         }
         // しゃがみ
-        else if (y < -0.5f && isGrounded)
+        else if (isGrounded && (input == 1 || input == 2 || input == 3))
         {
-            // 前しゃがみ
-            if (x > 0.5f)
+            Crouching(input);            
+        }
+        // 立ち状態
+        else if(isGrounded)
+        {
+            // 歩き
+            if (input == 4 || input == 6)
             {
-                Debug.Log("前しゃがみ");
+                Walking(input);
             }
-            // 後ろしゃがみ
-            else if (x < -0.5f)
-            {
-                Debug.Log("後ろしゃがみ");
-            }
-            // しゃがみ
+            // 直立
             else
             {
-                Debug.Log("しゃがみ");
+                Idleing();
             }
-        }
-        // 横移動
-        else if(x != 0 && isGrounded)
-        {
-            transform.Translate(new Vector3(x * speed, 0, 0) * Time.deltaTime);
-        }
 
-        Debug.Log("x: " + x + " y: " + y);
+        }
     }
 
     public void OnCollisionEnter2D(Collision2D collision)
@@ -125,5 +166,94 @@ public class PlayerController : MonoBehaviour
         {
             isGrounded = true;
         }
+    }
+
+    public void Walking(int input)
+    {
+        // アニメーション更新
+        playerAnim.SetBool("isCrouching", false);
+        playerAnim.SetBool("isJumping", false);
+        playerAnim.SetBool("isJumpAttacking", false);
+        playerAnim.SetBool("isWalking", true);
+
+        // 移動
+        if (input == 6)
+        {
+            transform.Translate(new Vector3(speed, 0, 0) * Time.deltaTime);
+        }
+        else if(input == 4)
+        {
+            transform.Translate(new Vector3(-speed, 0, 0) * Time.deltaTime);
+        }
+        
+    }
+    public void Idleing()
+    {
+        // アニメーション更新
+        playerAnim.SetBool("isCrouching", false);
+        playerAnim.SetBool("isJumping", false);
+        playerAnim.SetBool("isJumpAttacking", false);
+        playerAnim.SetBool("isWalking", false);
+    }
+    public void Crouching(int input)
+    {
+        // アニメーション更新
+        playerAnim.SetBool("isWalking", false);
+        playerAnim.SetBool("isJumping", false);
+        playerAnim.SetBool("isJumpAttacking", false);
+        playerAnim.SetBool("isCrouching", true);
+
+        // 前しゃがみ
+        if (input == 3)
+        {
+            Debug.Log("前しゃがみ");
+        }
+        // 後ろしゃがみ
+        else if (input == 1)
+        {
+            Debug.Log("後ろしゃがみ");
+        }
+        // しゃがみ
+        else if(input == 2)
+        {
+            Debug.Log("しゃがみ");
+        }
+    }
+    public void Jumping(int input)
+    {
+        // アニメーション更新
+        playerAnim.SetBool("isCrouching", false);
+        playerAnim.SetBool("isWalking", false);
+        playerAnim.SetBool("isJumpAttacking", false);
+        playerAnim.SetBool("isJumping", true);
+
+        // 前ジャンプ
+        if (input == 9)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(new Vector2(5, 12), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+        // 後ろジャンプ
+        else if (input == 7)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(new Vector2(-5, 12), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+        // 垂直ジャンプ
+        else if (input == 8)
+        {
+            Rigidbody2D rb = GetComponent<Rigidbody2D>();
+            rb.AddForce(new Vector2(0, 12), ForceMode2D.Impulse);
+            isGrounded = false;
+        }
+    }
+    public void JumpAttack()
+    {
+        // アニメーション更新
+        playerAnim.SetBool("isJumpAttacking", true);
+
+        // 攻撃
     }
 }
